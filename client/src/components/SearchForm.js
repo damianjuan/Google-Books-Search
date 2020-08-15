83c3696ba3d547d78fc7265c8337a71c8835de16
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-15 10:28:36 -0500

    search component complete

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Card, Typography, List, ListItem, TextField } from '@material-ui/core';
+import { Paper, Card, Typography, List, ListItem, TextField, Button, Input, InputLabel, FormControl } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,7 +17,6 @@ const useStyles = makeStyles((theme) => ({
         },
         input: {
             margin: theme.spacing(1),
-            width: '90px',
         },
 
     },
@@ -34,10 +33,15 @@ export default function SearchForm() {
                         <ListItem style={{ justifyContent: 'left' }} >
                             <Typography variant="h6" className={classes.title}>Books Search</Typography>
                         </ListItem>
-                        <ListItem style={{ justifyContent: 'left' }}>
-                            <form noValidate autoComplete="off" className={classes.input}>
-                                <TextField id="outlined-basic" label="Search" variant="outlined" />
-                            </form>
+                        <ListItem>
+
+                            <FormControl fullWidth className={classes.margin} variant="filled">
+                                <InputLabel>Search</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </ListItem>
+                        <ListItem style={{ alignContent: 'right' }}>
+                            <Button className={classes.margin} variant="contained" color="primary">Search</Button>
                         </ListItem>
                     </List>
                 </Card>
