7d40754432325bfd3f7ba91cb342775225a0d6a2
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-15 03:16:39 -0500

    search form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
+import SearchForm from './components/SearchForm';
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Banner />
+
+      <SearchForm />
     </div>
   );
 }
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Card, Typography } from '@material-ui/core';
+import { Paper, Card, Typography, List, ListItem, TextField } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,6 +15,11 @@ const useStyles = makeStyles((theme) => ({
         title: {
             flexGrow: 1,
         },
+        input: {
+            margin: theme.spacing(1),
+            width: '90px',
+        },
+
     },
 }));
 
@@ -25,8 +30,16 @@ export default function SearchForm() {
         <div className={classes.root}>
             <Paper>
                 <Card className={classes.root}>
-                    <Typography align='center' variant="h4" className={classes.title}>(React) Google Books Search</Typography>
-                    <Typography align='center' variant="h6" className={classes.title}>Search for and Save Books of Interest</Typography>
+                    <List>
+                        <ListItem style={{ justifyContent: 'left' }} >
+                            <Typography variant="h6" className={classes.title}>Books Search</Typography>
+                        </ListItem>
+                        <ListItem style={{ justifyContent: 'left' }}>
+                            <form noValidate autoComplete="off" className={classes.input}>
+                                <TextField id="outlined-basic" label="Search" variant="outlined" />
+                            </form>
+                        </ListItem>
+                    </List>
                 </Card>
             </Paper>
 
