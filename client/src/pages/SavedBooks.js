b29a4c9adcab6f26a99e5413d40a162cec8705bb
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-16 22:07:15 -0500

    loadsavedbooks set up

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper, Typography } from '@material-ui/core';
 import SavedCard from '../components/SavedCard';
+import API from '../utils/API';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,24 +22,46 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function SearchResults() {
+export default function SavedBooks() {
     const classes = useStyles();
+    // Setting our component's initial state
+    const [books, setBooks] = useState([])
+
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    function loadBooks() {
+        API.getSavedBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err))
+        console.log(books);
+    }
+
 
     return (
         <div className={classes.root}>
             <Paper>
                 <Typography variant="h6" className={classes.title}>Saved Books</Typography>
                 <Grid container spacing={4}>
-                    {/* will need to add a map her to give each result thier own grid with card add back  key={card}  */}
-                    <Grid item xs={12} sm={6} md={4}>
-                        <SavedCard />
+
+                    {books.length >= 1 ?
+                        books.map(res =>
+                            <Grid item xs={12} sm={6} md={4}>
+                                <SavedCard /> {console.log(res)}
+                            </Grid>
+                        )
+                        :
+
+                        <Grid item xs={12} sm={6} md={4}>
+                            No Books Saved
                     </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <SavedCard />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <SavedCard />
-                    </Grid>
+
+                    }
+
                 </Grid>
             </Paper>
         </div>
