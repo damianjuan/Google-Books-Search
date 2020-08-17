3dfd590c68823bfd97465d6d7b0953066380dac8
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-16 20:01:38 -0500

    seed test

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,7 +8,7 @@ export default {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTerm);
     },
     saveBook: function (bookData) {
-        return axios.post("/api/books", bookData);
+        return axios.post("/api/books/", bookData);
     }
 
 
